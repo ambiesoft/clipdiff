d3e5afc344338273c069e697a729d8731fad9763
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-01-15 09:48:02 +0000

    iii
    git-svn-id: svn://192.168.0.99/clipdiff@6654 5ada3c48-002d-0410-850d-99865cc6fd0f

--- clipdiff/ListViewForScroll.cpp
+++ clipdiff/ListViewForScroll.cpp
@@ -5,24 +5,43 @@
 
 namespace clipdiff {
 	using namespace System::Windows::Forms;
-	
-	
+
+
 	ListViewForScroll::ListViewForScroll(void)
 	{
 
 	}
 
+	int getTopIndex(ListViewForScroll^ list)
+	{
+		ListViewItem^ top = list->TopItem;
+		if(!top)
+			return -1;
+
+		return list->Items->IndexOf(top);
+	}
+
+	bool working_;
 	void ListViewForScroll::WndProc(Message% m) 
 	{
 		switch(m.Msg)
 		{
-			case WM_VSCROLL:
+		case WM_VSCROLL:
 			{
-				//WORD pos = HIWORD(m.WParam.ToInt32());
-				//WORD scv = LOWORD(m.WParam.ToInt32());
+				if(working_)
+				{
+					DTRACE("WM_VSCROLL cancelled");
+					break;
+				}
+
+				WORD pos = HIWORD(m.WParam.ToInt32());
+				WORD scv = LOWORD(m.WParam.ToInt32());
 
 				//DTRACE("pos =" + pos);
-				//DTRACE("scv =" + scv);
+				DTRACE("scv =" + scv);
+
+				//if(scv==SB_THUMBTRACK)
+				//	break;
 
 				//ListView_Scroll((HWND)other_->Handle.ToPointer(), 0, pos);
 
@@ -36,37 +55,49 @@ namespace clipdiff {
 				////	pos,
 				////	TRUE);
 				ListView::WndProc(m);
-				::SendMessage((HWND)other_->Handle.ToPointer(),
+				::PostMessage((HWND)other_->Handle.ToPointer(),
 					WM_APP_LISTVIEWSCROLLPOSCHANGED,
 					0,0);
 				return;
 			}
 			break;
 
-			case WM_MOUSEWHEEL:
+		case WM_MOUSEWHEEL:
 			{
 				ListView::WndProc(m);
-				::SendMessage((HWND)other_->Handle.ToPointer(),
+				::PostMessage((HWND)other_->Handle.ToPointer(),
 					WM_APP_LISTVIEWSCROLLPOSCHANGED,
 					0,0);
 				return;
 			}
 			break;
 
-			case WM_APP_LISTVIEWSCROLLPOSCHANGED:
+		case WM_APP_LISTVIEWSCROLLPOSCHANGED:
 			{
+				if(m.WParam.ToInt32() > 10)
+					break;
+
+
 				if(other_->IsDisposed)
 					break;
 
-				ListViewItem^ top = other_->TopItem;
-				if(!top)
-					break;
-
-				int index = other_->Items->IndexOf(top);
+				int index = getTopIndex(other_);
 				if(index < 0)
 					break;
 
+				working_=true;
 				this->TopItem = this->Items[index];
+				working_=false;
+
+				if(index != getTopIndex(this))
+				{
+					::PostMessage((HWND)this->Handle.ToPointer(),
+					WM_APP_LISTVIEWSCROLLPOSCHANGED,
+					m.WParam.ToInt32()+1,
+					0);
+				}
+
+				// Application::DoEvents();
 			}
 			break;
 		}

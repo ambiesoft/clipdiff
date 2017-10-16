5ec606f0745565961e94b9d6813da935c655dc24
zelda <zelda@THEXP>
2017-10-16 22:38:08 +0900

    compress ruby

--- docdiff/ruby2.4/bin/rake.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Linkout\clipdiff\docdiff\ruby2.4\bin\ruby.exe" "C:/Linkout/clipdiff/docdiff/ruby2.4/bin/rake" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\Linkout\clipdiff\docdiff\ruby2.4\bin\ruby.exe" "%~dpn0" %*
